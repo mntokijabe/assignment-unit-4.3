844e99eb194a6bbd3e737749f4228e2049765346
Elwood Nelson <elwoodnelson@gmail.com>
2024-06-27 15:50:46 -0500

    stuck on trying to get empty function to be approved by test script

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,11 +2,50 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
 
 
+console.log(basket.length);
+function addItem(item){
+    basket.push(item);
+    return true;
+}  //end addItem function
+
+console.log(`Adding gum ${addItem("gum")}`);  //testing addItem function
+ console.log(basket);
+console.log(`Adding candy ${addItem("candy")}`);
+ console.log(basket);
+console.log(`Adding pop ${addItem("pop")}`);
+ console.log(basket);
+console.log(`Adding chips ${addItem("chips")}`);
+ console.log(basket);
+console.log(`Adding ice cream ${addItem("ice cream")}`);
+ console.log(basket);
+
+function listItems(cart){
+    console.log('In your basket you currently have:')
+    for (let entry of cart){
+        console.log(entry);
+    }
+}
+listItems(basket);
 
 
+function empty(array){
+    console.log(array);
+    console.log(array.length);
+    let cart1 = array
+ while (cart1.length > 0){
+        cart1.pop();
+        console.log(`inside array there are ${cart1.length} items`);
+       }
+    console.log(`the array is now ${array}`);
+    return cart1;
+}
 
+
+console.log(`this is what's left in your basket: ${empty(basket)}`);
 
 
 
